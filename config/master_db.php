ed348e88ebd9d104cf8e4c363d2570053aae276f
anib9871 <anib9871@gmail.com>
2026-09-26 19:19:14 +0530

    Update master_db.php

--- config/master_db.php
+++ config/master_db.php
@@ -1,12 +1,14 @@
 <?php
 // config/master_db.php
-$db_host = 'mysql.railway.internal'; // Railway ka private network host
-$db_port = '3306';
-$db_name = 'railway'; // Railway par default DB ka naam aksar 'railway' hota hai (Variables tab mein MYSQL_DATABASE dekh lena)
-$db_user = 'root';
-$db_pass = 'tAkeZegypoAxZNlmmMBnDHHKFdbWwmIe'; // Apna Railway Variables tab se copy kiya hua password yahan paste karo
+
+$db_host = getenv('MYSQLHOST');
+$db_port = getenv('MYSQLPORT');
+$db_name = getenv('MASTER_DB_NAME');
+$db_user = getenv('MYSQLUSER');
+$db_pass = getenv('MYSQLPASSWORD');
 
 try {
+
     $master_pdo = new PDO(
         "mysql:host={$db_host};port={$db_port};dbname={$db_name};charset=utf8mb4",
         $db_user,
@@ -17,6 +19,9 @@ try {
             PDO::ATTR_EMULATE_PREPARES   => false
         ]
     );
+
 } catch (PDOException $e) {
+
     die("Master DB Connection Failed: " . $e->getMessage());
+
 }
